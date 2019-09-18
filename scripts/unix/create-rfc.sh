076408d679f42996b353cfe54076f7990fcea8dc
Hans Koch <mail@hans-koch.me>
2019-09-18 23:28:20 +0200

    added template creation script for win and unix

---
+++ scripts/unix/create-rfc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+# ========== FUNCTIONS ==========
+# https://stackoverflow.com/questions/7126580/expand-a-possible-relative-path-in-bash/7126780
+function normalizepath {
+  (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
+}
+
+repo_folder=$(normalizepath "$(dirname "${BASH_SOURCE[0]}")/../")
+
+# ========== PROMPT =============
+
+echo "Enter Proposal ID (my-feature): "
+read proposal_id
+
+cp "${repo_folder}rfc-template.md" "${repo_folder}text/0000-${proposal_id}.md"
+echo "Created \"${repo_folder}text/0000-${proposal_id}.md\""
---
+++ scripts/windows/create-rfc.bat
@@ -0,0 +1,14 @@
+@echo off
+CALL :NORMALIZEPATH "%~dp0..\.."
+SET repo_folder=%RETVAL%
+
+:: ========== PROMPT =============
+set /p proposal_id="Enter Proposal ID (my-feature): "
+copy "%repo_folder%\rfc-template.md" "%repo_folder%\text\0000-%proposal_id%.md"
+echo Created "%repo_folder%\text\0000-%proposal_id%.md"
+:: ========== FUNCTIONS ==========
+exit /b
+
+:NORMALIZEPATH
+  set RETVAL=%~dpfn1
+  exit /b
